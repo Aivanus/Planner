967c71c054b23bb78f98809704978b49e763b076
Aivanus <ivank1995@hotmail.com>
2016-01-28 13:16:26 +0200

    koodaaminen aloitettu

---
+++ Planner/src/main/java/planner/domain/Day.java
@@ -0,0 +1,6 @@
+package planner.domain;
+
+public enum Day {
+
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ Planner/src/test/java/planner/domain/TimeTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planner.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ivan
+ */
+public class TimeTest {
+    
+    public TimeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+//    @Test
+//    public void inputIsConvertedToMinutesCorrectly(){
+//        Time time = new Time(0,0);
+//        assertEquals(0, time.getMinutes());
+//        
+//        time = new Time(2,0);
+//        assertEquals(120, time.getMinutes());
+//        
+//        time = new Time(0,2);
+//        assertEquals(2, time.getMinutes());
+//        
+//        time = new Time(24,0);
+//        assertEquals(1440, time.getMinutes());
+//    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
