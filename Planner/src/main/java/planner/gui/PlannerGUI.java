3eddda084911ee39c4da5ca8692723f3b370c04b
Aivanus <ivank1995@hotmail.com>
2016-02-22 21:33:05 +0200

    kenttien tyhjennysnappi lisätty

--- Planner/src/main/java/planner/gui/PlannerGUI.java
+++ Planner/src/main/java/planner/gui/PlannerGUI.java
@@ -283,6 +283,11 @@ public class PlannerGUI extends javax.swing.JFrame {
         );
 
         clearButton.setText("Clear");
+        clearButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -292,7 +297,9 @@ public class PlannerGUI extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(clearButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(clearButton)))
                 .addGap(28, 28, 28)
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
@@ -305,7 +312,7 @@ public class PlannerGUI extends javax.swing.JFrame {
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(clearButton)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
@@ -386,6 +393,16 @@ public class PlannerGUI extends javax.swing.JFrame {
 //            System.out.println(schedule.getValueAt(r, c));
 //        }
     }//GEN-LAST:event_scheduleMouseClicked
+
+    private void clearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearButtonActionPerformed
+        nameTextField.setText("New Task");
+        timeStartTextField.setText("00");
+        timeEndTextField.setText("00");
+        dayComboBox.setSelectedIndex(0);
+        currentColor = null;
+        chosenColor.setForeground(currentColor);
+        chosenColor.setBackground(currentColor);
+    }//GEN-LAST:event_clearButtonActionPerformed
 
     private void updateCell(Task task) {
         int time = task.getStartTime();
